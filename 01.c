7b4e872c60a7aea00627b3ace9b78e9c25324177
livingod <144990631+livingod@users.noreply.github.com>
2023-11-09 15:49:44 +0900

    05

--- 01.c
+++ 01.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <string.h>
-
 int main(int argc, char *argv[])
 {
-  char src[30] = "happy C programming";
-  
-  printf("문자열 \"%s\"의 길이: %i\n", src, strlen(src));
-    
+   FILE* fp;
+   
+   char str[100];//공간이야~~ 
+   int i;
+   
+   fp = fopen("sample.txt","w");
+   
+   for(i=0;i<3;i++)
+   {
+        printf("input a word: ");
+        scanf("%s",str);// & 안 써도 됩니다요 
+   
+        fprintf(fp, "%s\n", str);//fp 
+        }
+   
+   
+   
+   
+   fclose(fp);
+   
+   
   system("PAUSE");	
   return 0;
 }
